de78c05ff9331704897570bd6590a9157a8b49e0
Jowus <145287807+Jowus@users.noreply.github.com>
2025-03-11 22:00:29 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/main.css";
-import "./components/Characters";
-import "./components/Episodes";
+import Characters from "./components/Characters";
+import Episodes from "./components/Episodes";
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
-import Characters from "./components/Characters";
-import Episodes from "./components/Episodes";
-
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
- <React.StrictMode>
-   <Header />
-   <BrowserRouter>
-     <nav>
-       <ul>
-         <li>
-           <Link to="/characters">Przejdź do Characters</Link>
-         </li>
-         <li>
-           <Link to="/episodes">Przejdź do Episodes</Link>
-         </li>
-       </ul>
-     </nav>
-     <Routes>
-       <Route path="/characters" element={<Characters />} />
-       <Route path="/episodes" element={<Episodes />} />
-     </Routes>
-   </BrowserRouter>
-   <Footer />
- </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/characters">Przejdź do Characters</Link>
+          </li>
+          <li>
+            <Link to="/episodes">Przejdź do Episodes</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/episodes" element={<Episodes />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
